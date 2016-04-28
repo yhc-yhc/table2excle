dacb10ecf9aae903efa2b95bb9fdcd2450b310ff
yhc-yhc <yhc-yhc@users.noreply.github.com>
2016-04-28 11:47:23 +0800

    Create datasoure.js

---
+++ datasoure.js
@@ -0,0 +1,36 @@
+var rs=[{"SYS_NAME":"XXX",
+		"USER_NAME":"XXX",
+		"CHK_TIME":"XXX",
+		"DCTASK_NO":"201510231555534480000",
+		"DVC_LIST":[
+			{"DVC_RFID":"RFID-4046784230591726453728354",
+			"DVC_TYPE":20,
+			"BQLS":000001,
+			"DVC_ID":9,
+			"CHKITEMS":[{
+				"DCHK_TIME":"2015-10-23 16:03:42",
+				 "CHKITEM_KEY":"状态",
+				 "CHKITEM_VALUE":"故障",
+				}],
+
+		"TYPE_NAME":"X86服务器",
+		"PLACE_PATH":"机房-省中心-机柜2-1"},
+		{"DVC_RFID":"RFID-4046784230591726453728355",
+			"DVC_TYPE":20,
+			"BQLS":000002,
+			"DVC_ID":10,
+			"CHKITEMS":[{
+				"DCHK_TIME":"2015-10-23 16:03:42",
+				 "CHKITEM_KEY":"电流",
+				 "CHKITEM_VALUE":"10A",
+				},
+				{
+				"DCHK_TIME":"2015-10-23 16:03:42",
+				 "CHKITEM_KEY":"电压",
+				 "CHKITEM_VALUE":"220V",
+				}],
+
+		"TYPE_NAME":"P570服务器",
+		"PLACE_PATH":"机房-省中心-机柜2-2"},
+		]
+}]
